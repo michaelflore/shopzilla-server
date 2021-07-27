03a73c493bed1b503046298b18f92577275e3d6e
Flores <944163@cognizant.com>
2021-07-27 13:31:06 -0400

    Second commit.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = require('./app');
 
 // Connection URL
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://michael:' + process.env.PASSWORD + '@cluster0.kuznd.mongodb.net/' + process.env.PROJECT_NAME +'?retryWrites=true&w=majority', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).then(() => {
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).then(() => {
     console.log("Successfully connect to MongoDB.");
 })
 
